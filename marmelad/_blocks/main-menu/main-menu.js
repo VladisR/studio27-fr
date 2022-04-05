40a71be6c374cdac483dfa9270e4b5c9e74731b2
Vlad <vladis_@inbox.ru>
2022-04-05 20:13:49 +0500

    Шапка

--- marmelad/_blocks/main-menu/main-menu.js
+++ marmelad/_blocks/main-menu/main-menu.js
@@ -1,7 +1,7 @@
-$('.main-menu li').each(function(){
+$('.main-menu li, .header__menu li').each(function(){
     if($(this).children('ul').length) {
         $(this).addClass('has-child')
-        $(this).find('>a').append('<span class="m-tgl">')
+        // $(this).find('>a').append('<span class="m-tgl"><span class="svg-icon svg-icon--arr-d" aria-hidden="true"><svg class="svg-icon__link"><use xlink:href="#arr-d"></use></svg></span></span>')
     }
 });
 
