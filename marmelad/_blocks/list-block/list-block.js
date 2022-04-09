73d866fef494dbcb3b5ace97330f85947159a239
Vlad <vladis_@inbox.ru>
2022-04-09 16:37:18 +0500

    стр service-new

--- marmelad/_blocks/list-block/list-block.js
+++ marmelad/_blocks/list-block/list-block.js
@@ -19,9 +19,9 @@ var catalogSlider = new Swiper(".js-list-carousel", {
     breakpoints: {
       0: {
         slidesPerView: 1,
-        spaceBetween: 33
+        spaceBetween: 17
       },
-      360: {
+      375: {
         slidesPerView: 1,
         spaceBetween: 33
       },
@@ -37,10 +37,48 @@ var catalogSlider = new Swiper(".js-list-carousel", {
         slidesPerView: 4,
         spaceBetween: 43
       },
-      // 1600: {
-      //   slidesPerView: 5,
-      //   freeMode: true,
-      //   spaceBetween: 43
-      // }
+    }
+});
+
+
+var catalogSlider2 = new Swiper(".js-list-carousel2", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      hide: false,
+      draggable: true
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    spaceBetween: 33,
+    slidesPerView: 2,
+    slideActiveClass: 'is-active',
+    freeMode: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 17
+      },
+      375: {
+        slidesPerView: 1,
+        spaceBetween: 33
+      },
+      575: {
+        slidesPerView: 2,
+        spaceBetween: 33
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 33
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 43
+      },
     }
 });
