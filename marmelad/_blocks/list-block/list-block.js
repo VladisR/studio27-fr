54e12f8e8d6fe273e3a6c6fdb5579fabddf7e84e
Vlad <vladis_@inbox.ru>
2022-04-11 21:20:38 +0500

    +2 стр

--- marmelad/_blocks/list-block/list-block.js
+++ marmelad/_blocks/list-block/list-block.js
@@ -82,3 +82,49 @@ var catalogSlider2 = new Swiper(".js-list-carousel2", {
       },
     }
 });
+
+
+$('.js-list-carousel4').each(function(){
+  var slider = this;
+  var catalogSlider4 = new Swiper(slider, {
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: false,
+        draggable: true
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+      spaceBetween: 20,
+      slidesPerView: 2,
+      slideActiveClass: 'is-active',
+      freeMode: false,
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        360: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        660: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+        1921: {
+          slidesPerView: 4,
+          spaceBetween: 30
+        },
+      }
+  });
+});
